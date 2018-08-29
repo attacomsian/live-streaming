c30987a9b57288d356854384e274154c36f684e3
Atta-Ur-Rehman Shah <hi@attacomsian.com>
2018-08-29 11:35:14 +0500

    add user registration

--- src/main/java/com/streaming/Application.java
+++ src/main/java/com/streaming/Application.java
@@ -2,9 +2,16 @@ package com.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/streaming/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.streaming.controllers;
+
+import com.streaming.domains.User;
+import com.streaming.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(String username, String password) {
+        User user = new User(username, bCryptPasswordEncoder.encode(password));
+        userRepository.save(user);
+    }
+}
